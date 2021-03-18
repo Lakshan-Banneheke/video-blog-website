11d5a860dfcc74b75dc38f117e15824069c3a1ca
Lakshan <btlakshan@gmail.com>
2021-03-18 15:26:16 +0530

    added session store

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_profile CASCADE;
 DROP TABLE IF EXISTS vlogs CASCADE;
+DROP TABLE IF EXISTS session CASCADE;
 
 
 CREATE TABLE user_profile (
@@ -16,8 +17,20 @@ CREATE TABLE vlogs (
     uploadTimestamp TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
 GRANT ALL ON TABLE public.user_Profile to vlog_app;
 GRANT ALL ON TABLE public.vlogs to vlog_app;
+GRANT ALL ON TABLE public.session to vlog_app;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO vlog_app;
 
 INSERT into user_profile(username, password) VALUES('nadun', '$2a$10$kOW6.ySlaD3rXfV/FbWEPemyH328qPfOEM5zfg3AEP2pRBsHa50we');
